0cf179fb60e68c64d178a0aad6a475e1b30f9b56
Kevin Isabelle <kev.isabelle@gmail.com>
2016-04-11 21:22:09 -0400

    Piezo drum module update.

--- KFramework/projects/PiezoDrumLED/PiezoDrumLED.ino
+++ KFramework/projects/PiezoDrumLED/PiezoDrumLED.ino
@@ -1,18 +1,27 @@
 #include <Adafruit_NeoPixel.h>
  
-#define PIN       6
+#define STRIP1       6
+#define STRIP2       7
+#define STRIP3       8
+#define STRIP4       9
+#define STRIP5       10
+
 #define N_LEDS    30
-#define L1        5
-#define L2        7
-#define BTN1      A3
-#define BTN2      A4
-#define BTN3      A5
+#define L1        4
+#define L2        5
 
 #define SENSOR1   A1
 #define SENSOR2   A2
+#define SENSOR3   A3
+#define SENSOR4   A4
+#define SENSOR5   A5
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
- 
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(N_LEDS, STRIP1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(N_LEDS, STRIP2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(N_LEDS, STRIP3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip4 = Adafruit_NeoPixel(N_LEDS, STRIP4, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip5 = Adafruit_NeoPixel(N_LEDS, STRIP5, NEO_GRB + NEO_KHZ800);
+
 int param_colorIndex = 0;
 int param_mode = 0;
 int param_slope = 0;
@@ -20,76 +29,64 @@ int param_showPeaks = 0;
 
 int value1 = 0;
 int value2 = 0;
+int value3 = 0;
+int value4 = 0;
+int value5 = 0;
 
 void setup() {
   
-  pinMode(A0, INPUT);
   pinMode(L1, OUTPUT);
   pinMode(L2, OUTPUT);
-  pinMode(BTN1, INPUT);
-  pinMode(BTN2, INPUT);
-  pinMode(BTN3, INPUT);
-  pinMode(SENSOR1, INPUT);
   
-  strip.begin();
+  pinMode(SENSOR1, INPUT);
+  pinMode(SENSOR2, INPUT);
+  pinMode(SENSOR3, INPUT);
+  pinMode(SENSOR4, INPUT);
+  pinMode(SENSOR5, INPUT);
+  
+  strip1.begin();
+  strip2.begin();
+  strip3.begin();
+  strip4.begin();
+  strip5.begin();
+    
+  digitalWrite(L1, true);
 }
 
 void loop() {
   
-  bool btn1 = !analogRead(BTN1);
-  bool btn2 = !analogRead(BTN2);
-  bool btn3 = !analogRead(BTN3);
+  //getSensorsValue();
   
-  digitalWrite(L1, true);
-  delay(1000);
-  digitalWrite(L2, true);
-  delay(1000);
-  digitalWrite(L1, false);
-  delay(1000);
-  digitalWrite(L2, false);
-  delay(1000);
-  //digitalWrite(L2, btn3);
+  int sensor1 = analogRead(SENSOR1);
   
-  //value = analogRead(A0);
-  value = 0;
-  //strip.setPixelColor(value/4, strip.Color(0, 255, 0)); // Draw new pixel
-      //strip.setPixelColor(i-4, 0); // Erase pixel a few steps back
-   //  strip.show();
-   for (int i=0; i<1053; i++){
-  
-  setLevel(strip.Color(i/16, 254-(i/4), i/16), i);
-  //delay(2);
+  if (sensor1 < 80){
+      sensor1 = 0;
+  } else {
+    sensor1 = 1053 ;
   }
   
-  //chase(strip.Color(map(value, 0, 255, 0, 1053), 0, 0)); // Red
-  //chase(strip.Color(0, 255, 0)); // Green
-  //chase(strip.Color(0, 0, 255)); // Blue
+  if (value1 - sensor1 > 5){
+    value1 -= 5;  
+  } else {
+    value1 = sensor1;  
+  }
+  
+  setLevel(strip1.Color(map(value1, 0, 1053, 0, 255), 0, 255 - map(value1, 0, 1053, 0, 255)), value1);
+  
 }
  
 static void setLevel(uint32_t c, int value) {
-  
 
-  for(uint16_t i=0; i<strip.numPixels()+4; i++) {
-      strip.setPixelColor(i  , strip.Color(0, 0, 0)); // Draw new pixel
-      //strip.setPixelColor(i-4, 0); // Erase pixel a few steps back
-      
-      
+  for(uint16_t i=0; i<strip1.numPixels()+4; i++) {
+      strip1.setPixelColor(i  , strip1.Color(0, 0, 0)); // Draw new pixel
   }
   
- 
+  int maxLed = map(value,-1, 1053,  0, strip1.numPixels()+2);
   
-  int maxLed = map(value,-1, 1053,  0, strip.numPixels()+2);
-  
-  for(uint16_t i=0; i<maxLed; i++) {
-    
-      strip.setPixelColor(i  , c); // Draw new pixel
-      //strip.setPixelColor(i-4, 0); // Erase pixel a few steps back
-      
-      
+  for(uint16_t i=0; i<maxLed; i++) {  
+      strip1.setPixelColor(i  , c); // Draw new pixel
   }
   
-  strip.show();
+  strip1.show();
   
-  //delay(10);
- 
 }
